c0a197bb5a3488b8a8ab86b6ef272ea25b4ff3ca
amartripathi0 <theamartripathi@gmail.com>
2024-10-13 23:07:00 +0530

    add auth redirection

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,6 +1,13 @@
+import { getServerSession } from "next-auth";
 import Navbar from "@repo/ui/components/navbar";
-
-export default function Home() {
+import { authOptions } from "../lib/auth";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if(session?.user) {
+     redirect("/dashboard");
+  }
   return (
     <div className="h-screen">
       <Navbar />
